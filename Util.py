2091156fccbe094c4f7dd5241b7cddb0a1bbce4b
Richard C <higgsbosonseeker@gmail.com>
2017-01-04 16:37:26 -0800

    Add files via upload

--- Util.py
+++ Util.py
@@ -1,6 +1,11 @@
 import socket
+import os
 
 class Util(object):
+    
+    def get_path(self, relative_path):
+        full_path = os.path.dirname(os.path.abspath(__file__))
+        return(full_path + relative_path)
 
     def valid_addr(self, address):
        try:
